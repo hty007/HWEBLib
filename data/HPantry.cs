9cffaacb8e5d26f460aa5f9a9d1cf78e83a0f2e1
hty007 <hty007@yandex.ru>
2019-05-08 10:32:21 +0300

    Функционал Lines перенёс в HPantry. и добавил поле nameDir для выбора должения файла

--- data/HPantry.cs
+++ data/HPantry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace htyWEBlib.data
@@ -6,16 +7,18 @@ namespace htyWEBlib.data
     public static class HPantry
     {
         private const string Path = "Data";
+        private static string nameDir = null;
+        public static string NameDir { get => nameDir; set => nameDir = value; }
 
         static public void SaveData(string name, params IHData[] db)
         {
-            string path = GetName(name);
+            string fullNamePath = GetName(name,nameDir);
             if (!Directory.Exists(Path))// Создаем директорию!
                 Directory.CreateDirectory(Path);
             // создаем объект BinaryWriter
-            if (File.Exists(path))
-                File.Delete(path);
-            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.CreateNew)))
+            if (File.Exists(fullNamePath))
+                File.Delete(fullNamePath);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(fullNamePath, FileMode.CreateNew)))
             {
                 foreach (var data in db)
                 {
@@ -24,12 +27,12 @@ namespace htyWEBlib.data
             }
         }
 
-        static public void LoadData(string name, params IHData[] db)
+        static public void LoadData(string nameFile, params IHData[] db)
         {
-            string path = GetName(name);
-            if (!File.Exists(path))
-                throw new FileNotFoundException($"Файл отсутвует в файловой системе по указонному адресу.\n{path}");
-            using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+            string fullNamePath = GetName(nameFile, nameDir);
+            if (!File.Exists(fullNamePath))
+                throw new FileNotFoundException($"Файл отсутвует в файловой системе по указонному адресу.\n{fullNamePath}");
+            using (BinaryReader reader = new BinaryReader(File.Open(fullNamePath, FileMode.Open)))
             {
                 foreach (var data in db)
                 {
@@ -43,13 +46,45 @@ namespace htyWEBlib.data
         /// </summary>
         /// <param name="name">название данных</param>
         /// <returns>true - существует, false - не существует</returns>
-        static public bool FileExists(string name) => File.Exists(GetName(name));
+        static public bool FileExists(string name) => File.Exists(name);
         /// <summary>
         /// Составить имя файла с относительным путём
         /// </summary>
-        /// <param name="name">название данных</param>
+        /// <param name="nameFile">название данных</param>
         /// <returns></returns>
-        static public string GetName(string name, string path = Path) => string.Format("{1}/{0}.dbh", name, path);
+        static public string GetName(string nameFile, string expansion=null)
+        {
+            if (expansion == null)
+                expansion = "dbh";
+            if (nameDir == null)
+                return string.Format("{1}/{0}.{2}", nameFile, Path, expansion);
+            return string.Format("{2}/{1}/{0}.{3}", nameFile,nameDir, Path, expansion);
+        }
 
+        public static void SaveLines(string nameFile, params HLines[] lines)
+        {
+            string fileName = GetName(nameFile, nameDir);
+            if (!Directory.Exists(nameDir))// Создаем директорию!
+                Directory.CreateDirectory(nameDir);
+            using (StreamWriter sw = new StreamWriter(fileName, false, System.Text.Encoding.Default))
+            {
+                foreach (var line in lines)
+                    line.Save(sw);
+            }
+        }
+
+        public static void LoadLines(string nameFile, params HLines[] lines)
+        {
+            var result = new List<HLines>();
+            string fullNameFile = GetName(nameFile, "lin");
+            if (!File.Exists(fullNameFile))
+                throw new FileNotFoundException($"Файл отсутвует в файловой системе по указонному адресу.\n{fullNameFile}");
+            using (StreamReader sr = new StreamReader(fullNameFile, System.Text.Encoding.Default))
+            {
+                foreach (var line in lines)
+                    line.Load(sr);                  
+            }
+            lines = result.ToArray();
+        }
     }
 }
