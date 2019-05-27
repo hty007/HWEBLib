31085b220a4bab4936cae021544151858f0bb550
hty007 <hty007@yandex.ru>
2019-05-27 10:12:14 +0300

    Update SvgTag.cs

--- HTag/SvgTag.cs
+++ HTag/SvgTag.cs
@@ -1,16 +1,35 @@
 ﻿namespace htyWEBlib.Tag
 {
-    public class SvgTag : HTag
+    public class SvgTag : BuilderTag
     {
+        #region Конструкторы и поля
         public string Height { get => this["height"]; set => this["height"] = value; }
         public string Width { get => this["width"]; set => this["width"] = value; }
 
         public SvgTag() : base(TypeTAG.svg)
         {
         }
-        public SvgTag(string nameID) : this()
+        public SvgTag(string name) : this()
         {
-            SetNameID(nameID);
+            this["name"] = name;
+        }
+        public SvgTag(double height, double width, string name = null) : this()
+        {
+            if (name != null)
+                this["name"] = name;
+            Height = height.ToString();
+            Width = width.ToString();
+        }
+        #endregion
+        #region Функционал
+        public SvgContent Rect
+        #endregion
+    }
+
+    public class SvgContent : BuilderTag
+    {
+        public SvgContent(TypeTAG tag) : base(tag)
+        {
         }
     }
 }
