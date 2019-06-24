f8a9efddb859fb6be869fbc2527c40db04b7d9ef
Ренат <hty007@yandex.ru>
2019-06-24 20:49:58 +0300

    Вынес session в отдельный файл

---
+++ eduDisciplines/Session.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using htyWEBlib.data;
+using htyWEBlib.Geo;
+
+namespace htyWEBlib.eduDisciplines
+{
+    public class Session : IEnumerable<HPoint>, IHData
+    {
+        private List<HPoint> data;
+        public HPoint this[int index] { get => data[index]; set => data[index] = value; }
+        public int Count { get => data.Count; }
+        public SessionType Type { get; set; }
+        public int ID { get; internal set; }
+
+        public Session(int id = 0)
+        {
+            data = new List<HPoint>();
+            Type = SessionType.Point;
+            ID = id;
+        }
+        public void Add(double x, double y)
+        {
+            Add(new HPoint(x, y));
+        }
+        public void Add(params HPoint[] points)
+        {
+            data.AddRange(points);
+        }
+
+        public IEnumerator<HPoint> GetEnumerator()
+        {
+            return ((IEnumerable<HPoint>)data).GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<HPoint>)data).GetEnumerator();
+        }
+        public void Load(BinaryReader reader)
+        {
+            data.Clear();
+            int c = reader.ReadInt32();
+            for (int i = 0; i < c; i++)
+            {
+                HPoint p = new HPoint();
+                p.Load(reader);
+                data.Add(p);
+            }
+        }
+        public void Save(BinaryWriter writer)
+        {
+            writer.Write(Count);
+            foreach (var p in data)
+            {
+                p.Save(writer);
+            }
+        }
+
+        internal HPoint[] GetPoints()
+        {
+            return data.ToArray();
+        }
+
+        internal HPoint[] GetPoints(Func<HPoint, HPoint> convertPoint)
+        {
+            List<HPoint> points = new List<HPoint>();
+            foreach (HPoint p in data)
+            {
+                points.Add(convertPoint(p));
+            }
+            return points.ToArray();
+        }
+    }
+}
